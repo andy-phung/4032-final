87fe7640ef09c193164405d620276c1e6b97274b
andy <andyphung64@gmail.com>
2025-05-02 16:11:02 -0400

    progress check 1

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ import Album from "./components/Album.js";
 function App() {
   const selectedAlbum = useRef("");
 
-  
+  let albums = [];
+
+  for (const album_name in data) {
+    albums.push(<Album name={`${album_name} (${data[album_name]["release_year"]})`}/>)
+  }
 
   return (
     <div className="bg-black w-screen h-screen">
-      <div className="w-screen h-screen flex justify-center items-center">
-        <Album name={"Face The Sun (2022)"}/>
+      <div className="w-screen h-screen flex justify-center items-center flex-col">
+        {albums}
       </div>
     </div>
   );
